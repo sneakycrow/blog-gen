4c2a9d232358a83bc6e3ecd50d739cb129f8829c
Zachary E Sohovich <zach@sneakycrow.dev>
2019-12-26 16:03:02 -0800

    moved post traversal into its own func

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use walkdir::WalkDir;
 
-fn main() -> Result<(), ()> {
+fn traverse_posts_directory() -> Result<(), ()> {
   for entry in WalkDir::new("./posts")
     .follow_links(true)
     .into_iter()
-    .filter_map(|e| e.ok()) {
-      let file_name = entry.file_name().to_string_lossy();
-      if file_name.ends_with(".md") {
-        println!("{}", file_name);
-      }
+    .filter_map(|e| e.ok())
+  {
+    let file_name = entry.file_name().to_string_lossy();
+    if file_name.ends_with(".md") {
+      println!("{}", file_name);
     }
-    Ok(())
+  }
+  Ok(())
+}
+
+fn main() {
+  traverse_posts_directory().unwrap();
 }
