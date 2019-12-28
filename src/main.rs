9f3b524713ba219566471a21286d4a2b9830ee82
Zachary E Sohovich <zach@sneakycrow.dev>
2019-12-27 20:14:52 -0800

    added yaml logic

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,39 @@ use walkdir::WalkDir;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::error::Error;
+use serde_derive::{Deserialize};
 
-fn open_post(file: File) {
+#[derive(Debug, PartialEq, Deserialize)]
+struct YamlHeader {
+    title: String,
+    author: String,
+    #[serde(default)]
+    published: bool
+}
+
+fn open_post(file: File) -> Result<(), Box<dyn Error>> {
   let mut buf_reader = BufReader::new(file);
   let mut contents = String::new();
   match buf_reader.read_to_string(&mut contents) {
     Err(e) => println!("Error: {:?}", e),
-    _ => println!("{:?}", contents)
+    _ => {
+      // println!("{:?}", contents);
+      let end_of_yaml = contents[4..].find("---").unwrap() + 4;
+      let yaml = &contents[..end_of_yaml];
+      println!("{:?}", yaml);
+      let YamlHeader {
+        author,
+        title,
+        published
+      } = serde_yaml::from_str(yaml)?;
+      println!("{:?}", YamlHeader { author, published, title });
+    }
   }
+  Ok(())
 }
 
-fn traverse_posts_directory() -> Result<(), ()> {
+fn traverse_posts_directory() -> Result<(), Box<dyn Error>> {
   for entry in WalkDir::new("./posts")
     .follow_links(true)
     .into_iter()
@@ -20,12 +42,9 @@ fn traverse_posts_directory() -> Result<(), ()> {
   {
     let file_path = entry.path();
     let file_name = entry.file_name().to_string_lossy();
-    let file = File::open(file_path);
+    let file = File::open(file_path)?;
     if file_name.ends_with(".md") {
-      match file {
-        Ok(file) => open_post(file),
-        Err(error) => println!("Error: {:?}", error)
-      }
+      open_post(file).unwrap();
     }
   }
   Ok(())
